ba6b720c6a86f464729c44531d7311ddc12e6861
Fogashi <mateus.luna01@fatec.sp.gov.br>
2024-07-02 22:11:10 -0300

    Adicionar arquivos de projeto.

---
+++ MochiCoin/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MochiCoin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MochiCoin/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MochiCoin
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
